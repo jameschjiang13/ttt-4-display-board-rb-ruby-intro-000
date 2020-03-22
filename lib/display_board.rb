7a297f8cdacf19bc49b62b82e569bbeeeca2986b
James Jiang <monochrome473@gmail.com>
2020-03-22 01:34:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,11 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(spot)
-  puts " #{spot[0]} | #{spot[1]} | #{spot[2]}"
+  puts " #{spot[0]} | #{spot[1]} | #{spot[2]} "
   puts "-----------"
-  puts " #{spot[3]} | #{spot[4]} | #{spot[5]}"
+  puts " #{spot[3]} | #{spot[4]} | #{spot[5]} "
   puts "-----------"
-  puts " #{spot[6]} | #{spot[7]} | #{spot[8]}"
+  puts " #{spot[6]} | #{spot[7]} | #{spot[8]} "
   puts "-----------"
 end
-
 board = ["O"," "," ","O"," "," "," ","X"," "]
 print display_board(board)
