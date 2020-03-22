6de4bad6ad5200e211e256951d912f0fa9675450
James Jiang <monochrome473@gmail.com>
2020-03-22 01:26:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 ini_board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]}"
+  puts "#{board[0]} | #{board[1]} | #{board[2]}"
   puts "-----------"
 
 end
